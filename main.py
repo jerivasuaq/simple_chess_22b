a4de4eb59a37b80c67692f709e6d3ed229a4aa2d
jerivas <jerivas@uaq.edu.mx>
2023-01-13 18:58:48 -0600

    Create a board class

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
-from piece import Piece
-from pawn import Pawn
+from board import Board
 
 if __name__ == '__main__':
-    p = Piece(0,0,'w')
-    pw = Pawn(1,1,'w')
-    pw.move_to(2,1)
-    pw.print()
-
-    txt_board = [
-        "        ",
-        "PPPPPPPP",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "pppppppp",
-        "        ",
-    ]
-
-    board = []
-    for row in range(8):
-        board.append([None]*8) 
-
-    for row in range(8):
-        for col in range(8):
-            c = txt_board[row][col]
-    range(8):
-            if board[row][col] is None:
-                print(' ',end='')
-            else:
-                print(board[row][col],end='')
-        print()
+    b = Board()
+    print(b)
 
     print("🏁🏁🏁")
