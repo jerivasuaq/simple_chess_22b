93706d5fe315daf507d269b09397ac0a7b8f9ec3
Diego Arana <darana2487@gmail.com>
2023-01-17 21:05:33 -0600

    Created rook class

---
+++ rook.py
@@ -0,0 +1,29 @@
+from piece import Piece
+
+class Rook(Piece):
+    def __init__(self, row, col, colour) -> None:
+        super().__init__(row, col, colour)
+
+    def move_to(self, row, col):
+        if self.colour == 'w':
+            if self.row + 1 == row:
+                self.row = row
+                return True
+        else:
+            if self.row - 1 == row:
+                self.row = row
+                return True
+
+        return False
+
+    def print(self):
+        print(self.__str__())
+
+    def __str__(self) -> str:
+        if self.colour=='w':
+            return '♜'
+        else:
+            return '♖'
+
+    def __repr__(self):
+        return self.__str__()
