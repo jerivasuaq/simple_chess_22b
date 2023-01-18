9815dfe8ae27f61d1d4d622328a69a79dff25ff8
alber91 <albertobebert@gmail.com>
2023-01-18 09:36:40 -0600

    Add king and rook initial status

--- king.py
+++ king.py
@@ -1,17 +1,25 @@
 from piece import Piece
-from tower import Tower #importing for class Tower
+#from rook import Rook #importing for class Tower
 
-class King(Piece, Tower):
+class King(Piece):
     
 
-    def __init__(self, row, col, colour, check, check_arr) -> None: #add check and check_arr to class Piece 
-        super().__init__(row, col, colour, check, check_arr)
+    def __init__(self, row, col, colour) -> None:  
+        super().__init__(row, col, colour)
 
     
     def move_to(self, row, col):
         mov_col=0
         mov_row=0
         mov=[]
+        originkb = True
+        originkw = True
+        originrrw = True
+        originrlw = True
+        originrrb = True
+        originrlb = True
+        check_arr = [(2,3), (1,5)]
+        check = False
     #     check_aux=[]
     #     ch if self.row != 0 and self.row != 7 and self.col != 0 and self.col != 7:
     #         for x in eck_status=False
@@ -22,13 +30,17 @@ class King(Piece, Tower):
                     
     #                 self.col=y
         if self.colour == 'w':
-            if self.row == 0 and self.col == 4: #checkin initial position of white king for castling
-                if self.col + 2 == col and self.check == False and origintrw == True: #kingside castling
+            if self.row == 0 and self.col == 4 and originkw == True: #checkin initial position of white king for castling
+                if self.col + 2 == col and check == False and originrrw == True: #kingside castling
                     if self.col + 1 == " " and self.col + 2 == " ": #checking empty spaces
                         mov_col = col #castle
-                elif self.col - 2 == col and self.check == False and origintlw == True: #queenside castling
+                        mov_row = row
+                        originrrw = False
+                elif self.col - 2 == col and check == False and originrlw == True: #queenside castling
                     if self.col + 1 == " " and self.col + 2 == " " and self.col + 3 == " ": #checking empty spaces
                         mov_col = col #castle
+                        mov_row = row
+                        originrlw = False
             
             if row - self.row <= 1 and col - self.col <= 1:
                 mov=[row,col]
@@ -36,28 +48,35 @@ class King(Piece, Tower):
                     if mov not in check_arr:
                         mov_row = row
                         mov_col = col
-            
-            self.row == mov_row
-            self.col == mov_col    
-                
-        else:
-            if self.row == 7 and self.col == 4: #checkin initial position of white king for castling
-                if self.col + 2 == col and self.check == False and origintrw == True: #kingside castling
-                    if self.col + 1 == " " and self.col + 2 == " ": #checking empty spaces
-                        mov_col = col #castle
-                elif self.col - 2 == col and self.check == False and origintlw == True: #queenside castling
-                    if self.col + 1 == " " and self.col + 2 == " " and self.col + 3 == " ": #checking empty spaces
-                        mov_col = col #castle
-            
-            if row - self.row <= 1 and col - self.col <= 1:
-                mov=[row,col]
-                for z in (len(check_arr)):
-                    if mov not in check_arr:
-                        mov_row = row
-                        mov_col = col
-            
             self.row == mov_row
             self.col == mov_col 
+            originkw = False   
+                
+        else:
+            if self.row == 7 and self.col == 4 and originkb == True: #checkin initial position of white king for castling
+                if self.col + 2 == col and check == False and originrlb == True: #kingside castling
+                    if self.col + 1 == " " and self.col + 2 == " ": #checking empty spaces
+                        mov_col = col #castle
+                        mov_row = row
+                        originrlb = False
+                elif self.col - 2 == col and check == False and originrrb == True: #queenside castling
+                    if self.col + 1 == " " and self.col + 2 == " " and self.col + 3 == " ": #checking empty spaces
+                        mov_col = col #castle
+                        mov_row = row
+                        originrrb = False
+
+            
+            if row - self.row <= 1 and col - self.col <= 1:
+                mov=[row,col]
+                for z in (len(check_arr)):
+                    if mov not in check_arr[z]:
+                        mov_row = row
+                        mov_col = col
+            self.row == mov_row
+            self.col == mov_col 
+            originkw = False             
+
+
 
         return False
 
