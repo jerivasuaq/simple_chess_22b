e24df219622969de678c3019806cf7818651be55
alber91 <albertobebert@gmail.com>
2023-01-18 01:08:53 -0600

    Update the king class

--- king.py
+++ king.py
@@ -1,34 +1,63 @@
 from piece import Piece
-from tower import Tower
+from tower import Tower #importing for class Tower
 
 class King(Piece, Tower):
-    originkw=False
-    originkb=False
+    
 
-    def __init__(self, row, col, colour, check) -> None: #añadir check a la clase Piece
-        super().__init__(row, col, colour, check)
+    def __init__(self, row, col, colour, check, check_arr) -> None: #add check and check_arr to class Piece 
+        super().__init__(row, col, colour, check, check_arr)
 
-
+    
     def move_to(self, row, col):
-
+        mov_col=0
+        mov_row=0
+        mov=[]
+    #     check_aux=[]
+    #     ch if self.row != 0 and self.row != 7 and self.col != 0 and self.col != 7:
+    #         for x in eck_status=False
+    #    range(self.row - 1, self.row + 1):
+    #             self.row=x
+    #             if self.row
+    #             for y in range(self.col - 1, self.col + 1):
+                    
+    #                 self.col=y
         if self.colour == 'w':
-            if self.row == 0 and self.col == 4: #comprobando posición inicial del rey blanco
-                originkw=True
-            if originkw == True and self.row + 2 == row and self.check == False:
-                if origintw == True:
-                    self.row
-
-
-            if self.row + 1 == row:
-                self.row = row
-                return True
+            if self.row == 0 and self.col == 4: #checkin initial position of white king for castling
+                if self.col + 2 == col and self.check == False and origintrw == True: #kingside castling
+                    if self.col + 1 == " " and self.col + 2 == " ": #checking empty spaces
+                        mov_col = col #castle
+                elif self.col - 2 == col and self.check == False and origintlw == True: #queenside castling
+                    if self.col + 1 == " " and self.col + 2 == " " and self.col + 3 == " ": #checking empty spaces
+                        mov_col = col #castle
+            
+            if row - self.row <= 1 and col - self.col <= 1:
+                mov=[row,col]
+                for z in (len(check_arr)):
+                    if mov not in check_arr:
+                        mov_row = row
+                        mov_col = col
+            
+            self.row == mov_row
+            self.col == mov_col    
+                
         else:
-            if self.row == 7 and self.col == 4: #comprobando posición inicial del rey negro
-                originkb=True
-
-            if self.row - 1 == row:
-                self.row = row
-                return True
+            if self.row == 7 and self.col == 4: #checkin initial position of white king for castling
+                if self.col + 2 == col and self.check == False and origintrw == True: #kingside castling
+                    if self.col + 1 == " " and self.col + 2 == " ": #checking empty spaces
+                        mov_col = col #castle
+                elif self.col - 2 == col and self.check == False and origintlw == True: #queenside castling
+                    if self.col + 1 == " " and self.col + 2 == " " and self.col + 3 == " ": #checking empty spaces
+                        mov_col = col #castle
+            
+            if row - self.row <= 1 and col - self.col <= 1:
+                mov=[row,col]
+                for z in (len(check_arr)):
+                    if mov not in check_arr:
+                        mov_row = row
+                        mov_col = col
+            
+            self.row == mov_row
+            self.col == mov_col 
 
         return False
 
